baa214fd82d7eefe221570c09f1ff124347425a6
unknown <gambit-333@mail.ru>
2022-05-30 23:39:33 +0300

    Nices

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){  /* document - наша HTML структура */ /* Код будет запускаться, когда HTML структура будет готова(ready) - ready(function()  */
     $('.carousel__inner').slick({  /* с помощью библиотеки JQuery мы обращаемся к какому-то классу */
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
+        speed: 1200,/*  скорость, при помощи которой мы будем переключать наш слайдер/ как быстро */
+        adaptiveHeight: true,/*  слайдер будет подстраиваться под высоту картинок */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>', /* стрелки наши. Вместо текста мы вве тэг img и добавли картинку */
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',  /* Путь к картинке прописать так, будт омы находимся в index.html | так работает система */
+        responsive: [ /* как себя слайдер будет вести на определенных промежутках */
             {
-                breakpoint: 992,
+                breakpoint: 992, /* На каком промежутке пикселей странице мы будем устанавливать эти правила */
                 settings: {
-                    dots: true,
-                    arrows: false
+                    dots: true, /* точки/кружочки внизу под слайдером */
+                    arrows: false /* стрелки на таком разрешении отключаю */
                 }
             }
         ]
